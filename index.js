966213b83ee8416b14d86cb426e9943e37fc1af4
Dnica98 <daniela.nica98@gmail.com>
2024-10-02 20:10:57 +0300

    1

---
+++ index.js
@@ -0,0 +1,152 @@
+// const primulCuvantRestuToate = {};
+// const CONSTANTE = 'denumirea compartiment';
+// const SNAKE_CASE = 'text';
+// const Capital = 'test';
+// const kebab_case = 'denumirea claselor';
+
+// const NUME = 'Daniela'
+
+// console.log('My name'+':'+' '+'Daniela')
+
+// console.log('My name is: ${NUME} and I use ${SNAKE_CASE}')
+
+
+// const str = '6'
+
+// console.log(typeof +str, +str)
+// console.log(typeof Number(str), Number(str))
+// console.log(3+ +str)
+
+// console.log('don\'t go there')
+
+// const num = 'test';
+
+// console.log(typeof num.toString())
+
+
+// const str = '50';
+// const num = 50;
+
+// console.log(Number(str))
+// console.log(+str)
+
+// console.log(num.toString())
+
+// const str = 'str';
+
+// const condition = false;
+
+// if(typeof str === 'number'){
+//     console.log('pass')
+// } else {
+//     console.log('not pass')
+// }
+
+
+// const num = 10
+// const num2 = 20
+
+
+// if(num == num2){
+//     console.log('pass')
+// } else{
+//     console.log('not pass')
+// }
+
+
+// if(num === num2){
+//     console.log('pass')
+// } else{
+//     console.log('not pass')
+// }
+
+
+// if(num > num2){
+//     console.log('pass')
+// } else {
+//     console.log('not pass')
+// }
+
+// if(num < num2){
+//     console.log('pass')
+// } else {
+//     console.log('not pass')
+// }
+
+
+// if(num != num2){
+//     console.log('pass')
+// } else {
+//     console.log('not pass')
+// }
+
+
+// if(num !== num2){
+//     console.log('pass')
+// } else {
+//     console.log('not pass')
+// }
+
+
+// if(num >= num2){
+//     console.log('pass')
+// } else {
+//     console.log('not pass')
+// }
+
+
+// if(num <= num2){
+//     console.log('pass')
+// } else if(num2 = num) {
+//     console.log('not pass')
+// }
+
+// const condition1 = true;
+// const condition2 = false;
+
+
+// if( condition2 || condition2 || condition1) {
+//     console.log('pass')
+// }
+
+// const defaultName = 'User';
+// const userName = 'Jhon';
+
+// const usedName = userName || defaultName;
+
+// console.log(usedName)
+
+// console.log(true && false && true)
+
+// const condition = false;
+// const condition2 = false;
+// const condition3 = false;
+
+// const ternary = condition ? 'option1' : condition2 ? 'option2' : condition3 ? 'option3' : 'option4';
+
+
+// console.log(ternary)
+
+
+const condition1 = true;
+const condition2 = false;
+
+if (condition1 || condition2){
+    console.log('pass')
+} else {
+    console.log('not pass')
+}
+
+const Name = 'Vania';
+const Name2 = 'Iura';
+
+const usedName = Name || Name2;
+
+console.log(usedName)
+console.log(true && true && true)
+
+const condition3 = false;
+
+const ternary = condition1 ? 'option1' : condition2 ? 'option2' : condition3 ? 'option3' : 'option4';
+
+console.log(ternary)
